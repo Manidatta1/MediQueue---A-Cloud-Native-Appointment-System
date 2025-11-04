bd39377bb42ff6f8f34a63887dc13de4e0aa468c
Manidatta1 <157147661+Manidatta1@users.noreply.github.com>
2025-11-03 19:05:25 -0700

    Update database.py

--- authentication_service/database.py
+++ authentication_service/database.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://healthcare_admin:healthcareadmin123@postgres-service:5432/healthcaredatabase",
+    "postgresql://healthcare_admin:healthcareadmin123@postgres-service:5432/healthcare",
 )
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
